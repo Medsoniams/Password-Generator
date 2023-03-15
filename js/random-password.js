861e717a38857efd233a1cc67581a1293b087981
Bohdan Kariev <karevbogdan@gmail.com>
2023-03-15 14:48:14 +0200

    ---

--- js/random-password.js
+++ js/random-password.js
@@ -1,18 +1,36 @@
-(function () {
-	const lengthPasswordInput = document.querySelector(".password__length");
-	const lengthPasswordOutput = document.querySelector(".password__length-value");
-	const lengthPassword = lengthPasswordInput.value;
+const passwordOutput = document.querySelector("password__output");
+const lengthPasswordInput = document.querySelector(".password__length");
+const lengthPasswordOutput = document.querySelector(".password__length-value");
+
+lengthPasswordOutput.innerHTML = 20;
+let lengthPassword = 0;
+
+function getLengthPassword() {
+lengthPasswordOutput.innerHTML = lengthPasswordInput.value;
+lengthPassword = lengthPasswordInput.value;
+renderPassword();
+}
+// passwordOutput.innerHTML = "123"
+
+function renderPassword() {
+	console.log(lengthPassword)
+}
 
 
-	lengthPasswordInput.oninput = function() {
-		lengthPasswordOutput.innerHTML = this.value;
-	}
 
-	// lengthPasswordOutput.innerHTML = lengthPassword;
-	// lengthPasswordInput.addEventListener("input", (event) => {
-	// 	lengthPasswordOutput.innerHTML = lengthPassword;
-	// });
-	//
-	// console.log(lengthPassword)
-	// lengthPasswordInput.addEventListener("input", );
-})();
+lengthPasswordInput.addEventListener('input', getLengthPassword)
+
+getLengthPassword()
+renderPassword()
+// lengthPasswordInput.addEventListener('input', function() {
+// 	const numbers = '1234567890'
+// 	let random = Math.floor(Math.random() * numbers.length)
+// 	lengthPasswordOutput.innerHTML = lengthPasswordInput.value;
+// 	lengthPassword = lengthPasswordInput.value;
+// 	let result = '';
+// 	for(let i = 0; i < lengthPassword; i += 1) {
+// 		result += numbers[random]
+// 	}
+// 	console.log(typeof result);
+// 	passwordOutput.innerHTML = 'gjh';
+// })
